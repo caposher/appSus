0ecd397b8fa3b19971b88593941ca94f2a082fac
capos <caposher@gmail.com>
2021-11-10 15:15:34 +0200

    get info from notes service

---
+++ js/apps/keep/service/notes.service.js
@@ -0,0 +1,18 @@
+import { storageService } from '../../../services/async-storage-service.js';
+export const notesService = { getNotes };
+
+const NOTES_KEY = 'notes';
+const gList = [
+  '1Lorem ipsum dolor sit amet consectetur ',
+  '2Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima alias nobis illo esse iste eum culpa quaerat commodi excepturi aperiam suscipit praesentium incidunt, dignissimos voluptatem sit ea delectus, quos aut.',
+  '3Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima alias nobis illo esse iste eum culpa quaerat commodi excepturi aperiam suscipit praesentium incidunt, dignissimos voluptatem sit ea delectus, quos autorem ipsum dolor sit amet consectetur adipisicing elit. Minima alias nobis illo esse iste eum culpa quaerat commodi excepturi aperiam suscipit praesentium incidunt, dignissimos voluptatem sit ea delectus, quos aut.',
+  '4Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima alias nobis illo esse iste eum culpa quaerat commodi excepturi aperiam suscipit praesentium incidunt, dignissimos voluptatem sit ea delectus, quos aut.',
+  '5Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima alias nobis illo esse iste eum culpa quaerat commodi excepturi aperiam suscipit praesentium incidunt, dignissimos voluptatem sit ea delectus, quos aut.',
+  '6Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima alias nobis illo esse iste eum culpa quaerat commodi excepturi aperiam suscipit praesentium incidunt, dignissimos voluptatem sit ea delectus, quos aut.',
+];
+
+localStorage.setItem(NOTES_KEY, JSON.stringify(gList));
+
+function getNotes() {
+  return storageService.query(NOTES_KEY);
+}
--- js/pages/keep-app.cmp.js
+++ js/pages/keep-app.cmp.js
@@ -1,24 +1,21 @@
+import { notesService } from '../apps/keep/service/notes.service.js';
 import notePreview from '../apps/keep/cmps/note-preview.cmp.js';
 export default {
   name: 'keep-app',
   template: `
-    <section class="keep-app main-content flex justify-center">
+    <section v-if="notes" class="keep-app main-content flex justify-center">
         <div class="nots-container main-width">
-          <notePreview v-for="(txt, idx) in temp" :key="idx" :txt="txt"/>
+          <notePreview v-for="(note, idx) in notes" :key="idx" :txt="note"/>
         </div>
     </section>
   `,
   data() {
     return {
-      temp: [
-        '1Lorem ipsum dolor sit amet consectetur ',
-        '2Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima alias nobis illo esse iste eum culpa quaerat commodi excepturi aperiam suscipit praesentium incidunt, dignissimos voluptatem sit ea delectus, quos aut.',
-        '3Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima alias nobis illo esse iste eum culpa quaerat commodi excepturi aperiam suscipit praesentium incidunt, dignissimos voluptatem sit ea delectus, quos autorem ipsum dolor sit amet consectetur adipisicing elit. Minima alias nobis illo esse iste eum culpa quaerat commodi excepturi aperiam suscipit praesentium incidunt, dignissimos voluptatem sit ea delectus, quos aut.',
-        '4Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima alias nobis illo esse iste eum culpa quaerat commodi excepturi aperiam suscipit praesentium incidunt, dignissimos voluptatem sit ea delectus, quos aut.',
-        '5Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima alias nobis illo esse iste eum culpa quaerat commodi excepturi aperiam suscipit praesentium incidunt, dignissimos voluptatem sit ea delectus, quos aut.',
-        '6Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima alias nobis illo esse iste eum culpa quaerat commodi excepturi aperiam suscipit praesentium incidunt, dignissimos voluptatem sit ea delectus, quos aut.',
-      ],
+      notes: null,
     };
+  },
+  created() {
+    notesService.getNotes().then((notes) => (this.notes = notes));
   },
   components: {
     notePreview,
