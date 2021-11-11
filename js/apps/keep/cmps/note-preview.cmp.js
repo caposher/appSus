9017635ffacdfcd6da6138725a00871db67160f1
capos <caposher@gmail.com>
2021-11-11 08:07:40 +0200

    note have now different sizes

--- js/apps/keep/cmps/note-preview.cmp.js
+++ js/apps/keep/cmps/note-preview.cmp.js
@@ -1,8 +1,8 @@
 export default {
   props: ['note'],
   name: 'note-preview',
-  template: ` <section class="note-preview flex flex-column content-center space-between">
-                <div class="notes-container">
+  template: ` <section :class="noteSize" class="note-preview flex flex-column content-center space-between">
+                <div  class="notes-container">
                   <p>{{note.info.txt}}</p>
                 </div>
                 <div class="note-tools">
@@ -10,4 +10,15 @@ export default {
                   <img @click="$emit('edit',note.id)" src="imgs/edit.png"/>
                 </div>
               </section>`,
+
+  computed: {
+    noteSize() {
+      let noteSize;
+      const txtLen = this.note.info.txt.length;
+      if (txtLen < 100) noteSize = 'small-note';
+      else if (txtLen < 300) noteSize = 'medium-note';
+      else noteSize = 'large-note';
+      return noteSize;
+    },
+  },
 };
