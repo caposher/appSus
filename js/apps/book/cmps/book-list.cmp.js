32dc9be827b52971c7bc2d656fe25c4b3ed4e410
capos <caposher@gmail.com>
2021-11-13 22:07:52 +0200

    fix and clear the code

--- js/apps/book/cmps/book-list.cmp.js
+++ js/apps/book/cmps/book-list.cmp.js
@@ -1,34 +1,21 @@
 import bookPreview from '../../book/cmps/book-preview.cmp.js';
 
 export default {
-    props: ['books'],
-    template: `
+  props: ['books'],
+  template: `
         <ul class="book-list">
             <li v-for="book in books" :key="book.id" class="book-preview-container" >
-            <!-- <router-link :to="'/book/'+book.id"> <book-preview :book="book" />  -->
                 <book-preview :book = "book" @click.native = "openBookDetails(book.id)"/>
         </router-link>
-                <!-- <div class="actions"> -->
-                    <!-- <button @click="remove(car.id)" >X</button> -->
-                    <!-- <button @click="select(book)" >Details</button> -->
-                    <!-- <router-link :to="'/car/'+car.id" >Details</router-link>
-                    <router-link :to="'/car/edit/'+car.id" >Edit</router-link> -->
-                <!-- </div> -->
             </li>
         </ul>
     `,
-    methods: {
-        // remove(carId) {
-        //     this.$emit('remove', carId);
-        // },
-        // select(book) {
-        //     this.$emit('selected', book);
-        // },
-        openBookDetails(bookId) {
-            this.$router.push('/book/' + bookId);
-        }
+  methods: {
+    openBookDetails(bookId) {
+      this.$router.push('/book/' + bookId);
     },
-    components: {
-        bookPreview
-    }
+  },
+  components: {
+    bookPreview,
+  },
 };
--- js/apps/mail/cmps/email-preview.cmp.js
+++ js/apps/mail/cmps/email-preview.cmp.js
@@ -1,10 +1,9 @@
-import { emailService } from "../service/email-service.js";
-// import { eventBus } from "../../../services/event-bus-service";
+import { emailService } from '../service/email-service.js';
 
 export default {
-    name: 'email-preview',
-    props: ['email'],
-    template: `
+  name: 'email-preview',
+  props: ['email'],
+  template: `
     <section>
     <li @click="openSmallPreview" class="email-preview-container" :class="classIsRead">
             <button @click.stop="toggleStar">
@@ -16,7 +15,6 @@ export default {
             <div class="sent-at"> {{ formattedDate }} </div>
             <div class="email-buttons">
                 <button @click.stop= "toggleRead"> <i :class="classStyleRead"></i> </button>
-                <!-- <i class="fas fa-envelope-open-text"></i> -->
                 <button @click= "deleteEmail(email.id)"> <i class="fas fa-trash"></i> </button>
             </div>
     </li>
@@ -27,7 +25,6 @@ export default {
                 <router-link :to="'email/' + email.id"> <i class="fas fa-expand"></i> </router-link>
                 <button @click.stop="replyToEmail(email)" > <i class="fas fa-reply"></i> </button>
                 <button @click.stop="saveAsNote(email)"> <i class="fas fa-paper-plane"></i> </button >
-                <!-- <button @click.stop="deleteEmail(email.id)" title = "Delete" > <i class="fas fa-trash"></i> </button > -->
             </div >
          </div >
          <p class="small-preview-from"> {{ email.from }} <span> <{{ email.fromEmail }}> </span> </p>
@@ -36,100 +33,86 @@ export default {
 </section>
     `,
 
-    data() {
-        return {
-            showEmail: false,
-            emailReply: null
+  data() {
+    return {
+      showEmail: false,
+      emailReply: null,
+    };
+  },
 
-        }
+  computed: {
+    formattedDate() {
+      var dateObj = new Date();
+      var month = dateObj.getUTCMonth() + 1; //months from 1-12
+      var day = dateObj.getUTCDate();
+      var year = dateObj.getUTCFullYear();
+      var newdate = day + '/' + month + '/' + year;
+      return new Date().toString().slice(4, 7) + ' ' + day; //month
     },
-
-    computed: {
-        formattedDate() {
-            var dateObj = new Date();
-            var month = dateObj.getUTCMonth() + 1; //months from 1-12
-            var day = dateObj.getUTCDate();
-            var year = dateObj.getUTCFullYear();
-            var newdate = day + "/" + month + "/" + year;
-            // return newdate;
-            return new Date().toString().slice(4, 7) + ' ' + day; //month
-            // var date = new Date(this.email.sentAt);
-            // return (this.email.sentAt) ? date.toLocaleString() : '';
-        },
-        formattedBodyText() {
-            return this.email.body.slice(0, 40);
-        },
-        formattedSubjectText() {
-            return this.email.subject.slice(0, 20);
-        },
-        classStyleRead() {
-            if (this.email.isRead) return 'fas fa-envelope-open'
-            else return 'fas fa-envelope'
-        },
-        classStar() {
-            if (this.email.isStarred) return 'fas fa-star checked'
-            else return 'far fa-star'
-        },
-        classIsRead() {
-            if (this.email.isRead) return 'read';
-            else return 'unread';
-        }
-
+    formattedBodyText() {
+      return this.email.body;
     },
-
-
-    methods: {
-        //move to dad?
-        toggleRead() {
-            console.log('test');
-            this.email.isRead = !this.email.isRead
-            emailService.updateEmail(this.email)
-                .then(email => this.email = email);
-        },
-        //move to dad?
-        toggleStar() {
-            console.log('test');
-            this.email.isStarred = !this.email.isStarred;
-            emailService.updateEmail(this.email)
-                .then(email => this.email = email);
-
-        },
-        deleteEmail(emailId) {
-            this.$emit('remove', emailId);
-        },
-        openSmallPreview() {
-            this.showEmail = !this.showEmail;
-            if (!this.email.isRead) this.email.isRead = true;
-            console.log(this.showEmail);
-        },
-        saveAsNote(email) {
-            this.$emit('saveAsNote', email);
-        },
-        replyToEmail(email) {
-            //            this.$router.push('/keep');
-            // console.log('this.$route.path', this.$route.path)
-            // console.log('this.$route.params', this.$route.params);
-            // console.log('this.$router.history.current.query', this.$router.history.current.query);
-            // // if (this.$route.path !== '/compose')
-            // //     this.$router.push(`compose?from=${email.fromEmail}&subject=Re:${email.subject}&body=${email.body}`);
-
-            // const { emailId } = this.$route.params;
-            // emailService.getById(emailId)
-            //     .then(email => {
-            //         this.emailReply = email;
-            //     })
-
-            // const email = this.emailReply;
-            this.emailReply = email;
-            console.log('this.emailReply', this.emailReply);
-            this.$router.push(`compose?to=${this.emailReply.to}&subject=Re:${this.emailReply.subject}&body=${this.emailReply.body}`);
-
-        }
+    formattedSubjectText() {
+      return this.email.subject;
     },
+    classStyleRead() {
+      if (this.email.isRead) return 'fas fa-envelope-open';
+      else return 'fas fa-envelope';
+    },
+    classStar() {
+      if (this.email.isStarred) return 'fas fa-star checked';
+      else return 'far fa-star';
+    },
+    classIsRead() {
+      if (this.email.isRead) return 'read';
+      else return 'unread';
+    },
+  },
 
+  methods: {
+    toggleRead() {
+      console.log('test');
+      this.email.isRead = !this.email.isRead;
+      emailService.updateEmail(this.email).then((email) => (this.email = email));
+    },
+    toggleStar() {
+      console.log('test');
+      this.email.isStarred = !this.email.isStarred;
+      emailService.updateEmail(this.email).then((email) => (this.email = email));
+    },
+    deleteEmail(emailId) {
+      this.$emit('remove', emailId);
+    },
+    openSmallPreview() {
+      this.showEmail = !this.showEmail;
+      if (!this.email.isRead) this.email.isRead = true;
+      console.log(this.showEmail);
+    },
+    saveAsNote(email) {
+      this.$emit('saveAsNote', email);
+    },
+    replyToEmail(email) {
+      //            this.$router.push('/keep');
+      // console.log('this.$route.path', this.$route.path)
+      // console.log('this.$route.params', this.$route.params);
+      // console.log('this.$router.history.current.query', this.$router.history.current.query);
+      // // if (this.$route.path !== '/compose')
+      // //     this.$router.push(`compose?from=${email.fromEmail}&subject=Re:${email.subject}&body=${email.body}`);
 
-    components: {
+      // const { emailId } = this.$route.params;
+      // emailService.getById(emailId)
+      //     .then(email => {
+      //         this.emailReply = email;
+      //     })
 
+      // const email = this.emailReply;
+      this.emailReply = email;
+      console.log('this.emailReply', this.emailReply);
+      this.$router.push(
+        `compose?to=${this.emailReply.to}&subject=Re:${this.emailReply.subject}&body=${this.emailReply.body}`
+      );
+    },
+  },
 
-    }
-}
+  components: {},
+};
