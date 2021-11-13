ccc0ce5bb833112285470614a0d411b1cfe25db6
capos <caposher@gmail.com>
2021-11-13 16:32:03 +0200

    responsive mail

--- js/apps/mail/cmps/email-compose.cmp.js
+++ js/apps/mail/cmps/email-compose.cmp.js
@@ -1,15 +1,15 @@
-import { emailService } from "../service/email-service.js";
-import { eventBus } from "../../../services/event-bus-service.js";
+import { emailService } from '../service/email-service.js';
+import { eventBus } from '../../../services/event-bus-service.js';
 
 export default {
-    props: ['composeEmail'],
-    template: `
+  props: ['composeEmail'],
+  template: `
         <section v-if="composeEmail" class="email-compose">
             <div class="compose-header">
                 <h3> New Message </h3> 
                 <button @click="exitAndSaveDraft" class="exit-btn"> <i class="fas fa-times"></i> </button>
             </div>
-            <form class="input-container" @submit.prevent="sendEmail">
+            <form class="email-input-container" @submit.prevent="sendEmail">
                 <div class="compose-to"> 
                     <input class="input-email" ref="to" type="email" v-model="email.to" placeholder="To" @input="debounce(saveDraft,5000)"/>    
                 </div>
@@ -29,94 +29,77 @@ export default {
         </section>
     `,
 
-    data() {
-        return {
-            email: null,
-            // saveDraft: false
-            isSaveDraft: false
+  data() {
+    return {
+      email: null,
+      // saveDraft: false
+      isSaveDraft: false,
+    };
+  },
+  created() {
+    this.email = {
+      to: '',
+      subject: '',
+      body: '',
+    };
+  },
 
-        }
+  monuted() {
+    this.$refs.to.focus();
+  },
+  methods: {
+    // exitAndSaveDraft() {
+    //     // this.saveDraft();
+    //     // this.$router.push('/email');
+    //     // composeEmail = false;
+    //     this.$emit('close');
+    //     this.$router.push('/email')
+
+    sendEmail() {
+      this.$emit('send', this.email);
+      this.$router.push('/email');
     },
-    created() {
-        this.email = {
-            to: '',
-            subject: '',
-            body: ''
-        }
+    deleteCompose() {
+      this.$emit('deleteCompose');
+      this.$router.push('/email');
     },
-
-    monuted() {
-        this.$refs.to.focus();
-
+    exitAndSaveDraft() {
+      this.saveDraft();
+      this.$emit('close');
+      this.$router.push('/email');
     },
-    methods: {
-        // exitAndSaveDraft() {
-        //     // this.saveDraft();
-        //     // this.$router.push('/email');
-        //     // composeEmail = false;
-        //     this.$emit('close');
-        //     this.$router.push('/email')
-
-
-        sendEmail() {
-            this.$emit('send', this.email);
-            this.$router.push('/email')
-        },
-        deleteCompose() {
-            this.$emit('deleteCompose');
-            this.$router.push('/email')
-
-        },
-        exitAndSaveDraft() {
-            this.saveDraft();
-            this.$emit('close');
-            this.$router.push('/email');
-        },
-        debounce(func, time) {
-            if (this.timeout) clearTimeout(this.timeout);
-            this.timeout = setTimeout(func, time);
-        },
-        saveDraft() {
-            if (this.email.to || this.email.subject || this.email.body) {
-                this.email.isDraft = true;
-                // emailService.saveEmailDraft(this.email)
-                emailService.sendEmail(this.email)
-                    .then(() => {
-                        this.isSaveDraft = true;
-                        const msg = {
-                            txt: `Email was added to Drafts`,
-                            type: 'success'
-                        };
-                        eventBus.$emit('showMsg', msg);
-                        setTimeout(() => {
-                            this.isSaveDraft = false;
-                        }, 5000);
-                    })
-                    .catch(err => {
-                        console.log('err', err);
-                        const msg = {
-                            txt: 'Error. Please try later',
-                            type: 'error'
-                        };
-                        eventBus.$emit('showMsg', msg);
-                    })
-
-
-                // this.$router.push('/email');
-            }
-        },
+    debounce(func, time) {
+      if (this.timeout) clearTimeout(this.timeout);
+      this.timeout = setTimeout(func, time);
     },
+    saveDraft() {
+      if (this.email.to || this.email.subject || this.email.body) {
+        this.email.isDraft = true;
+        // emailService.saveEmailDraft(this.email)
+        emailService
+          .sendEmail(this.email)
+          .then(() => {
+            this.isSaveDraft = true;
+            const msg = {
+              txt: `Email was added to Drafts`,
+              type: 'success',
+            };
+            eventBus.$emit('showMsg', msg);
+            setTimeout(() => {
+              this.isSaveDraft = false;
+            }, 5000);
+          })
+          .catch((err) => {
+            console.log('err', err);
+            const msg = {
+              txt: 'Error. Please try later',
+              type: 'error',
+            };
+            eventBus.$emit('showMsg', msg);
+          });
 
-
-}
-
-
-
-
-
-
-
-
-
-
-
+        // this.$router.push('/email');
+      }
+    },
+  },
+};
--- js/apps/mail/pages/email-app.cmp.js
+++ js/apps/mail/pages/email-app.cmp.js
@@ -2,14 +2,13 @@ import { emailService } from '../service/email-service.js';
 import emailList from '../cmps/email-list.cmp.js';
 import emailFolderList from '../cmps/email-folder-list.cmp.js';
 import emailFilter from '../cmps/email-filter.cmp.js';
-import { eventBus } from "../../../services/event-bus-service.js";
-import emailCompose from "../cmps/email-compose.cmp.js";
-import { notesService } from "../../keep/service/notes.service.js"
-
+import { eventBus } from '../../../services/event-bus-service.js';
+import emailCompose from '../cmps/email-compose.cmp.js';
+import { notesService } from '../../keep/service/notes.service.js';
 
 export default {
-    template: `
-        <section v-if="emails" class="email-app app-main">
+  template: `
+        <section v-if="emails" class="email-app main-content">
             <email-folder-list @folderUpdate="openRelevantFolder" @compose="isCompose"
              :emails="emails"/>
 
@@ -23,201 +22,181 @@ export default {
             </div>
         </section>
     `,
-    data() {
-        return {
-            emails: null,
-            filterBy: {
-                emailStatus: '',
-                text: ''
-            },
-            folder: 'inbox',
-            composeEmail: false,
-            // emailToCompose: null,
-            unreadEmails: 0
-        };
+  data() {
+    return {
+      emails: null,
+      filterBy: {
+        emailStatus: '',
+        text: '',
+      },
+      folder: 'inbox',
+      composeEmail: false,
+      // emailToCompose: null,
+      unreadEmails: 0,
+    };
+  },
+
+  created() {
+    this.loadEmails();
+    // this.filterBy.emailStatus = '';
+  },
+  methods: {
+    loadEmails() {
+      emailService.query().then((emails) => {
+        // console.log(emails);
+        this.emails = emails;
+      });
     },
 
-    created() {
-        this.loadEmails();
-        // this.filterBy.emailStatus = '';
+    setFilter(filterBy) {
+      this.filterBy = filterBy;
     },
-    methods: {
-        loadEmails() {
-            emailService.query()
-                .then(emails => {
-                    // console.log(emails);
-                    this.emails = emails;
-                });
-        },
-
-        setFilter(filterBy) {
-            this.filterBy = filterBy;
-        },
-        removeEmailFromList(emailId) {
-            emailService.removeEmail(emailId)
-                .then(() => {
-                    this.emails = this.emails.filter(email => email.id !== emailId)
-                    // this.composeEmail = false;
-                    const msg = {
-                        txt: `Email was removed`,
-                        type: 'success'
-                    };
-                    eventBus.$emit('showMsg', msg);
-                })
-                .catch(err => {
-                    console.log('err', err);
-                    const msg = {
-                        txt: 'Error. Please try later',
-                        type: 'error'
-                    };
-                    eventBus.$emit('showMsg', msg);
-                })
-        },
-        openRelevantFolder(folder) {
-            this.folder = folder;
-            console.log('folder', folder);
-        },
-        isCompose() {
-            this.composeEmail = true;
-        },
-        closeComposeEmail() {
-            // this.emailToCompose = null;
-            this.composeEmail = false;
-        },
-        sendAsNote(email) {
-            var note = notesService.getEmptyNote();
-            note.isPinned = true;
-            note.info.txt = `Email from ${email.from} + Subject - ${email.subject}` + '\n' +
-                `Message: "${email.body}"`;
-            notesService.addNote(note);
-            this.$router.push('/keep');
-
-
-        },
-        sendEmail(email) {
-            email.isSent = true;
-            emailService.sendEmail(email)
-                .then(() => {
-                    // console.log(email);
-                    // this.emails = this.emails.filter(email => email.isSent)
-                    this.composeEmail = false;
-                    this.folder = 'sent';
-                    this.emails = this.emails.filter(email => email.isSent)
-
-                    const msg = {
-                        txt: `Email was sent`,
-                        type: 'success'
-                    };
-                    eventBus.$emit('showMsg', msg);
-                })
-                .catch(err => {
-                    console.log('err', err);
-                    const msg = {
-                        txt: 'Error. Please try later',
-                        type: 'error'
-                    };
-                    eventBus.$emit('showMsg', msg);
-                })
-        },
-        toggleMenu() {
-            this.openNav = !this.openNav
-        },
-
+    removeEmailFromList(emailId) {
+      emailService
+        .removeEmail(emailId)
+        .then(() => {
+          this.emails = this.emails.filter((email) => email.id !== emailId);
+          // this.composeEmail = false;
+          const msg = {
+            txt: `Email was removed`,
+            type: 'success',
+          };
+          eventBus.$emit('showMsg', msg);
+        })
+        .catch((err) => {
+          console.log('err', err);
+          const msg = {
+            txt: 'Error. Please try later',
+            type: 'error',
+          };
+          eventBus.$emit('showMsg', msg);
+        });
     },
-
-    computed: {
-        emailsToShow() {
-
-            // console.log('filterBy.emailStatus', this.filterBy);
-            // var emailsToShow = this.emails;
-            if (!this.filterBy) return this.emails;
-            // console.log('this.filterBy.text', this.filterBy.text);
-            const searchStr = this.filterBy.text.toLowerCase();
-            // console.log('searchStr', searchStr);
-            var emailsToShow = this.emails;
-            // console.log('emailsToShow', emailsToShow);
-
-            // console.log('emailsToShow', emailsToShow);
-
-            if (this.filterBy.emailStatus !== '') {
-
-                if (this.filterBy.emailStatus === 'read')
-                    emailsToShow = emailsToShow.filter(email => email.isRead);
-                else if (this.filterBy.emailStatus === 'unread')
-                    emailsToShow = emailsToShow.filter(email => !email.isRead);
-                // console.log('emailsToShow', emailsToShow);
-
-                // return emailsToShow;
-            }
-            //
-            // folders addition // added
-            if (this.folder === 'inbox') {
-                emailsToShow = emailsToShow.filter(email => !email.isSent && !email.isDeleted && !email.isDraft);
-            }
-            //changed
-            else if (this.folder === 'starred') {
-                emailsToShow = emailsToShow.filter(email => email.isStarred);
-            }
-            else if (this.folder === 'sent') {
-                emailsToShow = emailsToShow.filter(email => email.isSent && !email.isDeleted);
-            }
-            else if (this.folder === 'trash') {
-                emailsToShow = this.emails.filter(email => email.isDeleted);
-                console.log('emailsIsTrashed', emailsToShow);
-            }
-            else if (this.folder === 'drafts') {
-                emailsToShow = this.emails.filter(email => email.isDraft && !email.isSent);
-                console.log('emailsIsDrafts', emailsToShow);
-            }
-            // emailsToShow = emailsToShow.filter(email =>
-            //     (email.body.toLowerCase().includes(searchStr)) || (email.subject.toLowerCase().includes(searchStr)));
-
-            emailsToShow = emailsToShow.filter(email =>
-                (email.body.toLowerCase().includes(searchStr)) || (email.subject.toLowerCase().includes(searchStr)));
-            return emailsToShow;
-        },
-
-
-
-
-
-
+    openRelevantFolder(folder) {
+      this.folder = folder;
+      console.log('folder', folder);
     },
-    watch: {
-        folder() {
-            this.loadEmails();
-            var emailsShow;
-            if (this.folder === 'inbox') {
-                emailsShow = this.emails.filter(email => !email.isSent);
-                this.emails = emailsShow;
-            }
-            else if (this.folder === 'starred') {
-                emailsShow = this.emails.filter(email => email.isStarred);
-                this.emails = emailsShow;
-                console.log(emailsShow);
-            }
-            else if (this.folder === 'sent') {
-                emailsShow = this.emails.filter(email => email.isSent);
-                this.emails = emailsShow;
-                console.log(emailsShow);
-            }
-            else if (this.folder === 'drafts') {
-                emailsShow = this.emails.filter(email => email.isDraft);
-                this.emails = emailsShow;
-                console.log(emailsShow);
-            }
-
-            console.log(emailsShow);
-            // return emailsShow;
-        }
+    isCompose() {
+      this.composeEmail = true;
     },
+    closeComposeEmail() {
+      // this.emailToCompose = null;
+      this.composeEmail = false;
+    },
+    sendAsNote(email) {
+      var note = notesService.getEmptyNote();
+      note.isPinned = true;
+      note.info.txt = `Email from ${email.from} + Subject - ${email.subject}` + '\n' + `Message: "${email.body}"`;
+      notesService.addNote(note);
+      this.$router.push('/keep');
+    },
+    sendEmail(email) {
+      email.isSent = true;
+      emailService
+        .sendEmail(email)
+        .then(() => {
+          // console.log(email);
+          // this.emails = this.emails.filter(email => email.isSent)
+          this.composeEmail = false;
+          this.folder = 'sent';
+          this.emails = this.emails.filter((email) => email.isSent);
 
-    components: {
-        emailList,
-        emailFolderList,
-        emailFilter,
-        emailCompose
+          const msg = {
+            txt: `Email was sent`,
+            type: 'success',
+          };
+          eventBus.$emit('showMsg', msg);
+        })
+        .catch((err) => {
+          console.log('err', err);
+          const msg = {
+            txt: 'Error. Please try later',
+            type: 'error',
+          };
+          eventBus.$emit('showMsg', msg);
+        });
+    },
+    toggleMenu() {
+      this.openNav = !this.openNav;
+    },
+  },
 
-    }
+  computed: {
+    emailsToShow() {
+      // console.log('filterBy.emailStatus', this.filterBy);
+      // var emailsToShow = this.emails;
+      if (!this.filterBy) return this.emails;
+      // console.log('this.filterBy.text', this.filterBy.text);
+      const searchStr = this.filterBy.text.toLowerCase();
+      // console.log('searchStr', searchStr);
+      var emailsToShow = this.emails;
+      // console.log('emailsToShow', emailsToShow);
 
-}
+      // console.log('emailsToShow', emailsToShow);
+
+      if (this.filterBy.emailStatus !== '') {
+        if (this.filterBy.emailStatus === 'read') emailsToShow = emailsToShow.filter((email) => email.isRead);
+        else if (this.filterBy.emailStatus === 'unread') emailsToShow = emailsToShow.filter((email) => !email.isRead);
+        // console.log('emailsToShow', emailsToShow);
+
+        // return emailsToShow;
+      }
+      //
+      // folders addition // added
+      if (this.folder === 'inbox') {
+        emailsToShow = emailsToShow.filter((email) => !email.isSent && !email.isDeleted && !email.isDraft);
+      }
+      //changed
+      else if (this.folder === 'starred') {
+        emailsToShow = emailsToShow.filter((email) => email.isStarred);
+      } else if (this.folder === 'sent') {
+        emailsToShow = emailsToShow.filter((email) => email.isSent && !email.isDeleted);
+      } else if (this.folder === 'trash') {
+        emailsToShow = this.emails.filter((email) => email.isDeleted);
+        console.log('emailsIsTrashed', emailsToShow);
+      } else if (this.folder === 'drafts') {
+        emailsToShow = this.emails.filter((email) => email.isDraft && !email.isSent);
+        console.log('emailsIsDrafts', emailsToShow);
+      }
+      // emailsToShow = emailsToShow.filter(email =>
+      //     (email.body.toLowerCase().includes(searchStr)) || (email.subject.toLowerCase().includes(searchStr)));
+
+      emailsToShow = emailsToShow.filter(
+        (email) => email.body.toLowerCase().includes(searchStr) || email.subject.toLowerCase().includes(searchStr)
+      );
+      return emailsToShow;
+    },
+  },
+  watch: {
+    folder() {
+      this.loadEmails();
+      var emailsShow;
+      if (this.folder === 'inbox') {
+        emailsShow = this.emails.filter((email) => !email.isSent);
+        this.emails = emailsShow;
+      } else if (this.folder === 'starred') {
+        emailsShow = this.emails.filter((email) => email.isStarred);
+        this.emails = emailsShow;
+        console.log(emailsShow);
+      } else if (this.folder === 'sent') {
+        emailsShow = this.emails.filter((email) => email.isSent);
+        this.emails = emailsShow;
+        console.log(emailsShow);
+      } else if (this.folder === 'drafts') {
+        emailsShow = this.emails.filter((email) => email.isDraft);
+        this.emails = emailsShow;
+        console.log(emailsShow);
+      }
+
+      console.log(emailsShow);
+      // return emailsShow;
+    },
+  },
+
+  components: {
+    emailList,
+    emailFolderList,
+    emailFilter,
+    emailCompose,
+  },
+};
