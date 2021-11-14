f33f7e4a997252b040a6a573f4963f7c691aaff8
AnnaM317 <anya317@gmail.com>
2021-11-14 07:58:32 +0200

    chnages

--- js/pages/home-page.cmp.js
+++ js/pages/home-page.cmp.js
@@ -1,7 +1,7 @@
 export default {
   template: `
     <section class="app-home-page main-content">
-      <h1>Welcome to AppSus</h1>
+      <h1 class="appsus">Welcome to AppSus</h1>
    
     </section>
   `,
